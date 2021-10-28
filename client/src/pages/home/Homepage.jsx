4913783f8eca6cac133e1d54c898c2f3d793c418
mohamad <msiysinyuy@gmail.com>
2021-10-28 16:03:53 +0100

    the react app installed and the top bar complete

---
+++ client/src/pages/home/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Homepage.css'
+import {Home} from "@material-ui/icons"
+import {Topbar} from "../../components"
+
+const Homepage = () => {
+  return (
+
+    <div>
+    <Topbar />
+    <Home/>Home page
+    </div>
+  )
+}
+
+export default Homepage
