31c44d4179f853492a0c1aae8c210dccecc783c1
mohamad <msiysinyuy@gmail.com>
2021-10-29 16:41:05 +0100

    the share component complete

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Feed.css'
+import {Share} from '../../components'
 const Feed = () => {
   return (
-    <div className="social__feed">This is feed</div>
+    <div className="social__feed">
+    <div className="social__feedWrapper">
+      <Share />
+    </div>
+
+    </div>
   )
 }
 
